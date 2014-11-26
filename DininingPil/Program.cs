39e4a1751d40c906934623901a7459ace66b35ff
dinushaw <dinusha@Dinusha-MacBook-Pro.local>
2014-11-26 14:47:21 +1100

    Concurrent Utilities in C#

---
+++ DininingPil/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Dinusha.ConcurrancyUtilities;
+using Thread= System.Threading.Thread;
+
+namespace DininingPil
+{
+	class MainClass
+	{
+		static int philcount=0;
+		static Philosopher p1;
+		//Semaphore forks = new Semaphore(5);
+		static Semaphore[] forks = new Semaphore[]{new Semaphore(1),new Semaphore(1), new Semaphore(1), new Semaphore(1),new Semaphore(1)};
+		static object lockobject = new object ();
+
+		static Semaphore footman = new Semaphore (5);
+		//static object lockobjectleft = new object ();
+
+
+		public static void Main (string[] args)
+		{
+			/// Crating 5 philosophers 
+
+			Philosopher p1 = new Philosopher (philcount++, forks);
+			Philosopher p2 = new Philosopher (philcount++, forks);
+			Philosopher p3 = new Philosopher (philcount++, forks);
+			Philosopher p4 = new Philosopher (philcount++, forks);
+			Philosopher p5 = new Philosopher (philcount++, forks);
+
+
+		}
+
+
+
+
+
+	}
+}
---
+++ Dinusha.ConcurrancyUtilities/ActiveObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Thread = System.Threading.Thread;
+
+namespace Dinusha.ConcurrancyUtilities
+{
+	/// <summary>
+	/// Active objects are used insted of naked thereads, 
+	/// To use , Extend this class and override the Execute method. 
+	/// </summary>
+	abstract class ActiveObject
+	{
+		Thread activeThread;
+
+		/// <summary>
+		/// Constructor which initialise the activeThread object 
+		/// with 'Execute' method as a parameter 
+		/// </summary>
+		public ActiveObject ()
+		{
+			activeThread = new Thread (Execute);
+		}
+
+		/// <summary>
+		/// This metod is usef to Start the thread.
+		/// </summary>
+		public void Start(){
+			activeThread.Start ();
+		}
+
+
+		/// <summary>
+		/// Logic for the active object. should be overridden to use. 
+		/// </summary>
+		protected abstract void Execute ();
+
+
+		/// <summary>
+		/// Stop the thread , To prevent hariming other executing threads 'Interrupt'
+		/// is used insted of 'Abort'
+		/// </summary>
+		public void Stop(){
+			activeThread.Interrupt ();
+		}
+	}
+
+
+}
+
